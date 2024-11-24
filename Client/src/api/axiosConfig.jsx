d226734561801d937dc152ee8da25d272d33ffea
Varun Raj <varunraj0609@gmail.com>
2024-11-24 19:24:36 +0530

    "minute"

--- Client/src/api/axiosConfig.jsx
+++ Client/src/api/axiosConfig.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://d718-2403-7340-2-d660-2db4-b5d9-c55c-23ec.ngrok-free.app",
+  baseURL: "https://c8f9-2403-7340-2-d660-84a6-85c3-a59f-68ae.ngrok-free.app",
   headers: {
     "ngrok-skip-browser-warning": "true", // Optional to bypass certain ngrok headers
   },
