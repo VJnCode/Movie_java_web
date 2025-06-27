ee1594a21cf910326045733875d2fad18acd3d64
Varun Raj <vjcodes247@gmail.com>
2025-06-28 00:47:16 +0530

    upadting to deploy

--- Client/src/api/axiosConfig.jsx
+++ Client/src/api/axiosConfig.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://c8f9-2403-7340-2-d660-84a6-85c3-a59f-68ae.ngrok-free.app",
+  baseURL:  "https://eb7d-2403-7340-2-d660-6805-6de2-9560-848a.ngrok-free.app",
   headers: {
     "ngrok-skip-browser-warning": "true", // Optional to bypass certain ngrok headers
   },
